c0e0541384402275be56994dca9ecefdb15ad5fb
David <david@MacNoob.local>
2023-04-26 17:17:08 -0400

    managing probes training independently

--- buildOrdersSystem/zealotsRush.js
+++ buildOrdersSystem/zealotsRush.js
@@ -3,59 +3,110 @@ const { createSystem, taskFunctions } = require('@node-sc2/core');
 // const { distance } = require('@node-sc2/core/utils/geometry/point');
 const { CHARGE } = require('@node-sc2/core/constants/upgrade');
 const { Alliance } = require('@node-sc2/core/constants/enums');
-const { CANCEL_LAST } = require('@node-sc2/core/constants/ability');
-const { ASSIMILATOR, CYBERNETICSCORE, FORGE, GATEWAY, NEXUS, TWILIGHTCOUNCIL, ROBOTICSBAY, ROBOTICSFACILITY } = require('@node-sc2/core/constants/unit-type');
+const { CANCEL_LAST, CANCEL_QUEUEPASIVE } = require('@node-sc2/core/constants/ability');
+const { ASSIMILATOR, CYBERNETICSCORE, FORGE, GATEWAY, NEXUS, TWILIGHTCOUNCIL, ROBOTICSBAY, ROBOTICSFACILITY,
+  PROBE } = require('@node-sc2/core/constants/unit-type');
 
 const { build, upgrade } = taskFunctions;
 
-const WORKERS_PER_BASE = 19
+const WORKERS_PER_BASE = 20
+const MAX_ARMY_SIZE = 80
+
+// const buildOrder = [
+//   [15, build(GATEWAY)],
+//   [18, build(ASSIMILATOR)],
+//   [21, build(NEXUS)],
+//   [24, build(FORGE)],
+//   [28, build(ASSIMILATOR)],
+//   [30, build(CYBERNETICSCORE)],
+//   [34, build(ROBOTICSFACILITY)],
+//   [34, build(GATEWAY, 3)],
+//   [37, build(ROBOTICSBAY)],
+//   [40, build(ASSIMILATOR, 2)],
+//   [39, build(TWILIGHTCOUNCIL)],
+//   [70, build(NEXUS)],
+//   [75, build(ASSIMILATOR, 2)],
+//   [100, build(NEXUS)],
+//   [120, build(ASSIMILATOR, 2)],
+//   [130, build(ROBOTICSFACILITY, 2)],
+//   [140, build(GATEWAY, 3)],
+//   [150, build(NEXUS)],
+//   [170, build(ASSIMILATOR, 2)],
+// ]
 
 const buildOrder = [
   [15, build(GATEWAY)],
-  [18, build(ASSIMILATOR)],
-  [21, build(NEXUS)],
-  [24, build(FORGE)],
-  [28, build(ASSIMILATOR)],
-  [30, build(CYBERNETICSCORE)],
-  [34, build(ROBOTICSFACILITY)],
+  [16, build(ASSIMILATOR)],
+  [17, build(NEXUS)],
+  [18, build(FORGE)],
+  [19, build(ASSIMILATOR)],
+  [20, build(CYBERNETICSCORE)],
+  [21, build(ROBOTICSFACILITY)],
+  [22, build(GATEWAY, 3)],
+  [23, build(ROBOTICSBAY)],
+  [24, build(ASSIMILATOR, 2)],
+  [25, build(TWILIGHTCOUNCIL)],
+  [26, build(NEXUS)],
+  [27, build(ASSIMILATOR, 2)],
+  [28, build(NEXUS)],
+  [29, build(ASSIMILATOR, 2)],
+  [30, build(ROBOTICSFACILITY, 2)],
+  [31, build(GATEWAY, 3)],
+  [32, build(NEXUS)],
+  [33, build(ASSIMILATOR, 2)],
   [34, build(GATEWAY, 3)],
-  [37, build(ROBOTICSBAY)],
-  [40, build(ASSIMILATOR, 2)],
-  [39, build(TWILIGHTCOUNCIL)],
-  [70, build(NEXUS)],
-  [75, build(ASSIMILATOR, 2)],
+  [35, build(ROBOTICSFACILITY, 2)],
 ]
 
 const defaultOptions = {
   state: {
     armySize: 12,
-    buildCompleted: false,
+    bitchesPerBase: 22,
     noMoreWorkersPls: false
   },
 }
+
+const wishList = []
 
 const getNumeros = ({ minerals, vespene, foodCap, foodUsed, foodArmy, foodWorkers, idleWorkerCount, armyCount, warpGateCount, larvaCount }) => {
   return { minerals, vespene, foodCap, foodUsed, foodArmy, foodWorkers, idleWorkerCount, armyCount, warpGateCount, larvaCount }
 }
 
-// For the overcrowded bases
-function getDirtyBitches(ownBases, mineralBitches, units) {
-  let dirtyBitches = [];
+const repeat = (item, times) => {
+	let rslt = [];
+	for(let i = 0; i < times; i++) {
+  	rslt.push(item)
+  }
+  return rslt;
+}
+
+// bases that need workers
+function getNeedyBases(ownBases) {
+  let needyBases = [];
   ownBases.forEach(base => {
     const diff = base.assignedHarvesters - base.idealHarvesters
     if(diff > 0) {
-      const extraBitches = units.getClosest(base.pos, mineralBitches, diff);
-      dirtyBitches.push(...extraBitches)
+      needyBases.push({diff, base})
     }
+  })
+  return needyBases
+}
+
+// For the overcrowded bases
+function getBitchBitches(ownBases, bitches, units) {
+  let dirtyBitches = [];
+  const bitchedBases = getNeedyBases(ownBases)
+  bitchedBases.forEach(({ diff, base }) => {
+    const extraBitches = units.getClosest(base.pos, bitches, diff);
+    dirtyBitches.push(...extraBitches)
   })
   return dirtyBitches
 }
 
 async function onGameStart({ resources }) {
   const { units } = resources.get()
-  console.log(`onGameStart in zealotsRush`)
   units.getWorkers().forEach(worker => worker.labels.set('oldBitch', true));
-  units.getBases(Alliance.SELF)[0].labels.set('allYourBase', true)
+  console.log(`onGameStart in zealotsRush`)
 }
 
 async function onUnitIdle({ resources }, idleUnit) {
@@ -74,35 +125,56 @@ async function onUnitFinished({ resources }, newBuilding) {
   }
 
   if (newBuilding.isTownhall()) {
-    // no se si funciona
-    console.log('MAGIA here')
-    const oldBitches = units.withLabel('oldBitch');
-    const daBase = units.withLabel('allYourBase')[0];
-    const mineFields = units.getMineralFields();
-    const targetBaseField = units.getClosest(daBase.pos, mineFields, 1)[0];
-    return Promise.all(oldBitches.map(bitch => actions.gather(bitch, targetBaseField)))
+    console.log('expanded!')
+    const dirtyBitches = getBitchBitches(units.getBases(Alliance.SELF).filter(b => b.buildProgress >= 1), units.getMineralWorkers(), units)
+    if (dirtyBitches.length > 0) {
+      console.log(`MOVILIZING ${dirtyBitches.length} bitches`)
+      return Promise.all(dirtyBitches.map(bitch => actions.gather(bitch)))
+    }
   }
   
-  const dirtyBitches = getDirtyBitches(units.getBases(Alliance.SELF), units.getMineralWorkers(), units)
-  if (dirtyBitches.length > 0) {
-    console.log(`MOVILIZING ${dirtyBitches.length} bitches`)
-    return Promise.all(dirtyBitches.map(bitch => actions.gather(bitch)))
-  }
 }
 
-async function onUnitCreated({ resources }, newUnit) {
+async function onUnitCreated({ agent, resources }, newUnit) {
   const { actions, units, map } = resources.get();
-  
+  const { foodCap, foodUsed } = agent
+  const foodLeft = foodCap - foodUsed
+  const miaBases = units.getBases(Alliance.SELF)
   // actual units logic
   if (newUnit.isWorker()) {
-    return actions.gather(newUnit);
+    actions.gather(newUnit);
   } else if (newUnit.isCombatUnit()) {
-    return actions.attackMove(newUnit, map.getCombatRally());
+    actions.attackMove(newUnit, map.getCombatRally());
   }
 
   // other logic to do when a unit is created
-  const noMoreWorkersPls = units.getWorkers().length > (units.getBases(Alliance.SELF).length * WORKERS_PER_BASE)
+  const noMoreWorkersPls = units.getWorkers().length > (miaBases.length * this.state.bitchesPerBase)
   this.setState({ noMoreWorkersPls });
+  
+  const needyBases = getNeedyBases(miaBases)
+
+  if (noMoreWorkersPls) {
+    console.log(`no moer twerkers - foodcap: ${foodCap}`)
+    if(foodCap > 50 && foodCap < 100) {
+      console.log('set bitchesPerBase to 20')
+      this.setState({ bitchesPerBase: 20 })
+    } else if(foodCap > 100 && foodCap < 150) {
+      console.log('set bitchesPerBase to 16')
+      this.setState({ bitchesPerBase: 16 })
+    } else if(foodCap > 150) {
+      console.log('set bitchesPerBase to 14')
+      this.setState({ bitchesPerBase: 14 })
+    }
+  } else if (foodLeft > 2 * needyBases.length) {
+    // order needed workers in their base
+    needyBases.forEach(({ diff, base }) => {
+      console.log('training more bitches - ', diff)
+      const orders = repeat({ unitId: PROBE, production: base }, diff)
+      wishList.push(...orders)
+    })
+  } else {
+    console.log(`no action needed ${JSON.stringify({foodLeft, needyBasesLength: needyBases.length, noMoreWorkersPls})}`)
+  }
 }
 
 async function onStep({ agent, data, resources }) {
@@ -112,6 +184,7 @@ async function onStep({ agent, data, resources }) {
   const idleCombatUnits = units.getCombatUnits().filter(u => u.noQueue);
   if (idleCombatUnits.length > this.state.armySize) {
     // add to our army size, so each attack is slightly larger
+    if (this.state.armySize < MAX_ARMY_SIZE)
     this.setState({ armySize: this.state.armySize + 4 });
     const [enemyMain, enemyNat] = map.getExpansions(Alliance.ENEMY);
     
@@ -123,8 +196,15 @@ async function onStep({ agent, data, resources }) {
 
   // Why tho T.T
   if (this.state.noMoreWorkersPls) {
-    const bases = units.getBases(Alliance.SELF);
-    return Promise.all(bases.map(base => actions.do(CANCEL_LAST, base)))
+    const basesBuildingShit = units.getBases(Alliance.SELF).filter(b => b.abilityAvailable(CANCEL_QUEUEPASIVE)) // bases[0].abilityAvailable(207)
+    return Promise.all(basesBuildingShit.map(base => actions.do(CANCEL_QUEUEPASIVE, base)))
+  } else if (wishList.length > 0) {
+    const { unitId, production } = wishList[0]
+    if (agent.canAfford(unitId) && agent.hasTechFor(unitId)) {
+      wishList.shift()
+      console.log(`got your ${unitId}, babe ;)`)
+      return actions.train(unitId, production)
+    }
   }
   // if (this.state.noMoreWorkersPls) {
   //   console.log('CANCEL, Beach!!')
--- strategySystem/protoss.js
+++ strategySystem/protoss.js
@@ -3,22 +3,34 @@ const { createSystem, taskFunctions } = require('@node-sc2/core');
 const { Alliance } = require('@node-sc2/core/constants/enums');
 const { GATEWAY, NEXUS, ROBOTICSFACILITY, CYBERNETICSCORE, ROBOTICSBAY, ZEALOT, STALKER, COLOSSUS, IMMORTAL } = require('@node-sc2/core/constants/unit-type');
 const { EFFECT_CHRONOBOOSTENERGYCOST } = require('@node-sc2/core/constants/ability');
-const { PROTOSSGROUNDWEAPONSLEVEL1, PROTOSSSHIELDSLEVEL1, PROTOSSGROUNDARMORSLEVEL1, PROTOSSGROUNDWEAPONSLEVEL2, PROTOSSSHIELDSLEVEL2, PROTOSSSHIELDSLEVEL3 } = require('@node-sc2/core/constants/upgrade');
+const { PROTOSSGROUNDWEAPONSLEVEL1, PROTOSSSHIELDSLEVEL1, PROTOSSGROUNDARMORSLEVEL1,
+  PROTOSSGROUNDWEAPONSLEVEL2, PROTOSSSHIELDSLEVEL2, PROTOSSGROUNDARMORSLEVEL2,
+  PROTOSSGROUNDWEAPONSLEVEL3, PROTOSSSHIELDSLEVEL3, PROTOSSGROUNDARMORSLEVEL3,
+  EXTENDEDTHERMALLANCE } = require('@node-sc2/core/constants/upgrade');
 
 const { build, upgrade, ability } = taskFunctions;
 
+// money to save for upgrades and builds
+const FOR_THE_WATCH = 250
+
 const buildOrder = [
-  [45, upgrade(PROTOSSGROUNDWEAPONSLEVEL1)],
-  [46, upgrade(PROTOSSSHIELDSLEVEL1)],
-  [50, upgrade(PROTOSSGROUNDARMORSLEVEL1)],
-  [58, upgrade(PROTOSSGROUNDWEAPONSLEVEL2)],
-  [65, upgrade(PROTOSSSHIELDSLEVEL2)],
-  [78, upgrade(PROTOSSSHIELDSLEVEL3)],
+  [1, upgrade(PROTOSSGROUNDWEAPONSLEVEL1)],
+  [2, upgrade(PROTOSSSHIELDSLEVEL1)],
+  [3, upgrade(PROTOSSGROUNDARMORSLEVEL1)],
+  [4, upgrade(PROTOSSGROUNDWEAPONSLEVEL2)],
+  [5, upgrade(EXTENDEDTHERMALLANCE)],
+  [6, upgrade(PROTOSSSHIELDSLEVEL2)],
+  [7, upgrade(PROTOSSSHIELDSLEVEL3)],
+  [8, upgrade(PROTOSSGROUNDARMORSLEVEL2)],
+  [9, upgrade(PROTOSSGROUNDWEAPONSLEVEL3)],
+  [10, upgrade(PROTOSSGROUNDARMORSLEVEL3)],
 ]
 
 const defaultOptions = {
   state: {
-      targetMoney: 0
+      targetMoney: 0,
+      seekAndDestroy: false,
+      fullRetaliation: false,
   },
 }
 
@@ -26,52 +38,82 @@ const getNumeros = ({minerals, vespene, foodCap, foodUsed, foodArmy, foodWorkers
   return { minerals, vespene, foodCap, foodUsed, foodArmy, foodWorkers, idleWorkerCount, armyCount, warpGateCount, larvaCount }
 }
 
+function canTrain(unitID, agent) {
+  return agent.canAfford(unitID) && agent.hasTechFor(unitID)
+  // const gotSpace = agent.foodCap - agent.foodUsed - 4; // T.T
+}
+
 async function onUnitFinished({ resources }, newBuilding) { 
   console.log(`onUnitFinished in protoss`)
 }
 
-async function onGameStart() { 
+async function onGameStart({ resources }) { 
   console.log(`onGameStart in protoss`)
+  // Label enemy main
+  resources.get().map.getExpansions(Alliance.ENEMY)[0].labels.set('allYourBase', true)
 }
 
 async function onStep({ agent, data, resources }) {
   const { units, actions, map } = resources.get();
-  const { minerals, vespene } = agent
+  const { minerals, vespene, foodArmy } = agent
   
   const idleRoboticFac = units.getById(ROBOTICSFACILITY, { noQueue: true, buildProgress: 1 })[0];
-  // all gateways that are done building and idle
   const idleGateway = units.getById(GATEWAY, { noQueue: true, buildProgress: 1 })[0];
-  
-  const hasCyberneticCore = !!units.getById(CYBERNETICSCORE, { buildProgress: 1 })[0]
-  const hasRoboticsBay = !!units.getById(ROBOTICSBAY, { buildProgress: 1 })[0]
+  const spaceLeft = agent.foodCap - agent.foodUsed
 
-  if(idleRoboticFac) {
-    // hasTechFor
-    if (agent.canAfford(COLOSSUS) && hasRoboticsBay) {
+  if (minerals < FOR_THE_WATCH && foodArmy > 10) {
+    // console.log('for the watch')
+    return null
+  }
+
+  if(idleRoboticFac && spaceLeft) {
+    if (canTrain(COLOSSUS, agent) && spaceLeft >= 5) {
       actions.train(COLOSSUS, idleRoboticFac)
       console.log('training Colossus')
     }
-    else if (agent.canAfford(IMMORTAL)) {
+    else if (canTrain(IMMORTAL, agent) && spaceLeft >= 7) {
       actions.train(IMMORTAL, idleRoboticFac)
       console.log('training Immortal')
     }
   }
 
-  // if (idleGateway && minerals > this.state.targetMoney) {
-  if (idleGateway) {
-    if (agent.canAfford(STALKER) && hasCyberneticCore) {
+  if (idleGateway && spaceLeft >= 3) {
+    if (canTrain(STALKER, agent)) {
       actions.train(STALKER, idleGateway);
       console.log('training Stalker')
-    } else if (agent.canAfford(ZEALOT)) {
+    } else if (canTrain(ZEALOT, agent)) {
       actions.train(ZEALOT, idleGateway);
       console.log('training Zealot')
     }
   }
+
+  if (this.state.seekAndDestroy) {
+    const expansions = map.getExpansions()
+    console.log(`Watch out! ${expansions.length}`)
+  }
 }
 
-async function onUnitCreated(world, newUnit) {
-  // add `map` to the resources we're getting
-  const { actions, map, units } = world.resources.get();
+async function onUnitDestroyed({ agent, resources }, destroyedUnit) {
+  console.log(`Too bad for ${destroyedUnit.unitType}`)
+  if (destroyedUnit.isTownHall()) {
+    console.log('HOLD THE LINE')
+    const { actions, map, units } = resources.get()
+    const army = units.getCombatUnits()
+
+    return actions.attackMove(army, destroyedUnit.pos, true)
+  }
+}
+
+async function onEnemyFirstSeen({ agent, resources }, enemyUnit) {
+  // if (enemyUnit.labels.has('allYourBase')) {
+  if (enemyUnit.isTownHall()) {
+    console.log('ALL YOUR BASE ARE BELONG TO US')
+    this.setState({ seekAndDestroy: true })
+  }
+}
+
+async function onUnitCreated({ agent, resources }, newUnit) {
+  const { actions, map, units } = resources.get();
 
   const nexuses = units.getById(NEXUS);
   const busyGW = units.getById(GATEWAY, { noQueue: false, buildProgress: 1 });
@@ -80,20 +122,16 @@ async function onUnitCreated(world, newUnit) {
   nexuses.forEach(nexus => {
     if(nexus.energy >= 50) {
       if (busyRF.length > 0) {
+        console.log('doing chrono boosty on maquinitas :D')
         const robotThingToBoost = busyRF[Math.floor(Math.random()*busyRF.length)]
-        actions.do(EFFECT_CHRONOBOOSTENERGYCOST, nexus, {target: robotThingToBoost})
-        console.log('doing chrono boosty :D')
+        return actions.do(EFFECT_CHRONOBOOSTENERGYCOST, nexus, {target: robotThingToBoost})
       } else if (busyGW.length > 0) {
+        console.log('doing chrono boosty on aliens :D')
         const gatewayToBoost = busyGW[Math.floor(Math.random()*busyGW.length)]
-        actions.do(EFFECT_CHRONOBOOSTENERGYCOST, nexus, {target: gatewayToBoost})
+        return actions.do(EFFECT_CHRONOBOOSTENERGYCOST, nexus, {target: gatewayToBoost})
       }
     }
   })
-
-  if (this.state.targetMoney > world.agent.minerals) {
-    this.setState({targetMoney: this.state.targetMoney + 50})
-    console.log('we rich bich')
-  }
 }
 
 async function onUpgradeComplete({ resources }, newUpgrade) {
@@ -114,5 +152,7 @@ module.exports = createSystem({
     onUnitFinished,
     onUnitCreated,
     onUpgradeComplete,
-    buildComplete
+    buildComplete,
+    onUnitDestroyed,
+    onEnemyFirstSeen
 });
